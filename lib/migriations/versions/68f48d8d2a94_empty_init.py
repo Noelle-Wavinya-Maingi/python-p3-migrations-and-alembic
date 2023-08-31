d4c3f6234fbaebb3ee53cd0aebbebe45ccc5041c
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-08-31 15:24:16 +0300

    Complete

---
+++ lib/migriations/versions/68f48d8d2a94_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 68f48d8d2a94
+Revises: 
+Create Date: 2023-08-31 15:12:39.418085
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '68f48d8d2a94'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
